2e8690006ae25c4e426e0e7eb5ab2c97f1ec8462
S00225961 <s00225961@atu.ie>
2025-04-14 15:02:34 +0100

    remove user code

--- auth.js
+++ auth.js
@@ -20,5 +20,6 @@ export async function signOutRedirect() {
   const clientId = "1u2p66b14lorj0ctn1gu9o0rqa";  // Replace with your Client ID
   const logoutUri = "https://main.diedwkmusxaeo.amplifyapp.com/";  // Set this to your logout URI
   const cognitoDomain = "https://us-east-159gzqkxdi.auth.us-east-1.amazoncognito.com";  // Replace with your Cognito domain
+  await userManager.removeUser();
   window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
 };
