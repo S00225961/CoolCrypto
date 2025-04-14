90e53320cb6dc153e5c33c4ac760b9963fd58a4f
S00225961 <s00225961@atu.ie>
2025-04-14 08:27:48 +0100

    changes for cognito implementation

--- cognito.js
+++ cognito.js
@@ -1,19 +1,36 @@
-import { UserManager } from "https://cdn.skypack.dev/oidc-client-ts";
+import { UserManager } from "oidc-client-ts";
 
 const cognitoAuthConfig = {
-  authority: "https://<your-user-pool-domain>.auth.<region>.amazoncognito.com",
-  client_id: "<your-client-id>",
-  redirect_uri: "https://<your-app-domain>/callback.html",
-  response_type: "code",
-  scope: "openid email profile"
+    authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_59GZQkxdi",
+    client_id: "1u2p66b14lorj0ctn1gu9o0rqa",
+    redirect_uri: "https://lustrous-rolypoly-ec9620.netlify.app/",
+    response_type: "code",
+    scope: "email openid phone"
 };
 
-export const userManager = new UserManager(cognitoAuthConfig);
+// create a UserManager instance
+export const userManager = new UserManager({
+    ...cognitoAuthConfig,
+});
 
-export async function signOutRedirect() {
-  const clientId = cognitoAuthConfig.client_id;
-  const logoutUri = "https://<your-app-domain>/index.html";
-  const cognitoDomain = cognitoAuthConfig.authority;
+export async function signOutRedirect () {
+  const clientId = "1u2p66b14lorj0ctn1gu9o0rqa";
+  const logoutUri = "https://lustrous-rolypoly-ec9620.netlify.app/";
+  const cognitoDomain = "https://us-east-159gzqkxdi.auth.us-east-1.amazoncognito.com";
   localStorage.removeItem("id_token");
   window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+};
+
+export function isTokenValid(idToken) {
+  try {
+    const payload = JSON.parse(atob(idToken.split('.')[1]));
+    return payload.exp * 1000 > Date.now(); // Check expiration
+  } catch {
+    return false;
+  }
 }
+
+
+
+
+
